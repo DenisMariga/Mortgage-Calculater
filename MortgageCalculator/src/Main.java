d1f6913f05ef0c3149fbb999fd2937d15a1b77ba
Denis Mariga <denismariga50@gmail.com>
2022-09-07 23:07:59 +0300

    added loops and conditional statements

--- MortgageCalculator/src/Main.java
+++ MortgageCalculator/src/Main.java
@@ -5,13 +5,35 @@ public class Main {
     public static void main(String[] args) {
         final byte month=12;
         final byte percentage = 100;
+        int principle =0;
+        float interest=0;
+        float years=0;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter Principle: ");
-        int principle =scanner.nextInt();
+        while (true){
+        System.out.print("Enter Principle ($1K-$1M): ");
+        principle =scanner.nextInt();
+        if (principle >=1000 && principle <= 1000_000){
+            break;
+        }else {
+            System.out.print("Enter a value between 1,000 and 1000,000");
+        }
+        }
+        while (true){
         System.out.print("Enter Annual Interest Rate: ");
-        float interest = (scanner.nextFloat()/percentage)/month;
+        interest = (scanner.nextFloat()/percentage)/month;
+        if (interest > 0 && interest <=30){
+            break;
+        }else {
+            System.out.print("Enter a value greater than 0 and less than or equal to 30");
+        }
+        }
+        while (true){
         System.out.print("Period in Years: ");
-        float years = scanner.nextFloat()*month;
+         years = scanner.nextFloat()*month;
+         if (years >=1 && years <=30)
+             break;
+            System.out.println("Enter a value between 1 and 30");
+        }
         float mortgage = (float) (principle*(interest* Math.pow(1+interest,years)/(Math.pow(1+interest,years)-1)));
         System.out.println("Mortgage: "+ NumberFormat.getCurrencyInstance().format(mortgage));
     }
